4fee2325cb8f3c4fc9a0ae56bb1c1ac5603dbf88
TMD Studios <tmdstudioscontact@gmail.com>
2021-04-01 01:33:58 +0400

    Added login buttons and ticker

--- app/src/main/java/com/tmdstudios/cryptoledger/MainActivity.java
+++ app/src/main/java/com/tmdstudios/cryptoledger/MainActivity.java
@@ -2,13 +2,41 @@ package com.tmdstudios.cryptoledger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button createProfileBtn;
+    private Button loginBtn;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.scrollingText);
+        textView.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        textView.setSelected(true);
+
+        createProfileBtn = findViewById(R.id.createProfile);
+        createProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        loginBtn = findViewById(R.id.login);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
